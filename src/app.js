9cc4a1903fb0bcc688071bd5449939aa2cfe28d6
SavyC93 <SavyC93@gmail.com>
2023-01-02 15:15:15 -0600

    added city in API

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "3ed82e3bd8722b160448f1693f7570dd";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
